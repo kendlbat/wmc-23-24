c97ca817f07b9eb77d06cadf3c2805cb54328238
KENDLBACHER Tobias, 4BHIF <kendlbat@edu.htl-villach.at>
2024-03-04 11:10:58 +0100

    Demo form

--- react-basics/src/school/form-intro/DemoForm.tsx
+++ react-basics/src/school/form-intro/DemoForm.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 const DemoForm: React.FC = () => {
-    return <></>;
+    const [user, setUser] = useState<{
+        username: string;
+        lastname: string;
+    }>({
+        username: "",
+        lastname: "",
+    });
+
+    return (
+        <Form
+            onSubmit={() => {
+                alert(JSON.stringify(user));
+            }}
+            action="javascript:void(0);">
+            <Form.Group>
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                    name="username"
+                    type="text"
+                    value={user.username}
+                    onChange={(e) => {
+                        setUser({ ...user, username: e.target.value });
+                    }}></Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Lastname</Form.Label>
+                <Form.Control
+                    name="lastname"
+                    type="text"
+                    value={user.lastname}
+                    onChange={(e) =>
+                        setUser({ ...user, lastname: e.target.value })
+                    }></Form.Control>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    );
 };
 
 export default DemoForm;
